b101111d6ec79ac779ca8992a7c449d5722f793a
ncocana <ncocana@cifpfbmoll.eu>
2023-02-18 20:33:47 +0100

    feat(TokenContract): create class

---
+++ src/main/java/edu/craptocraft/enzinium/TokenContract.java
@@ -0,0 +1,65 @@
+package edu.craptocraft.enzinium;
+
+import java.security.PublicKey;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TokenContract {
+    
+    private Map<PublicKey, Double> balances = new HashMap<>();
+    private String name = "";
+    private String symbol = "";
+    private Double totalSupply = 0d;
+    private Double tokenPrice = 0d;
+    private Address owner = null;
+
+    public TokenContract(Address owner) {
+        this.owner = owner;
+    }
+
+    public Address getOwner() {
+        return this.owner;
+    }
+
+    public Map<PublicKey, Double> getBalances() {
+        return this.balances;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String newName) {
+        this.name = newName;
+    }
+
+    public String getSymbol() {
+        return this.symbol;
+    }
+
+    public void setSymbol(String newSymbol) {
+        this.symbol = newSymbol;
+    }
+
+    public Double getTotalSupply() {
+        return this.totalSupply;
+    }
+
+    public void setTotalSupply(double newTotalSupply) {
+        this.totalSupply = newTotalSupply;
+    }
+    
+    public Double getTokenPrice() {
+        return this.tokenPrice;
+    }
+
+    public void setTokenPrice(double newTokenPrice) {
+        this.tokenPrice = newTokenPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "\nName: " + getName() + "\nSymbol: " + getSymbol() + "\nTotal Supply: " + getTotalSupply();
+    }
+
+}
