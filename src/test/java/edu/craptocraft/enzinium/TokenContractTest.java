dbc59a4892a85574cad2f3c262c513b2012a1349
ncocana <ncocana@cifpfbmoll.eu>
2023-02-19 00:01:52 +0100

    test(TokenContractTest): test payable method

--- src/test/java/edu/craptocraft/enzinium/TokenContractTest.java
+++ src/test/java/edu/craptocraft/enzinium/TokenContractTest.java
@@ -28,6 +28,11 @@ public class TokenContractTest {
         contract.addOwner(ownerTwo.getPK(), 50d);
         assertEquals(2, contract.getBalances().size());
 
+        ownerThree = new Address();
+        ownerThree.generateKeyPair();
+
+        contract.setTokenPrice(5d);
+
     }
     
     // Verifies that the owner's balance is correct.
@@ -57,13 +62,43 @@ public class TokenContractTest {
         assertEquals(98d, contract.balanceOf(ownerOne.getPK()), 0d);
         assertEquals(52d, contract.balanceOf(ownerTwo.getPK()), 0d);
 
+        // There are not enough units. Fails silently.
+        contract.transfer(ownerTwo.getPK(), 500d);
+        assertEquals(98d, contract.balanceOf(ownerOne.getPK()), 0d);
+        assertEquals(52d, contract.balanceOf(ownerTwo.getPK()), 0d);
+
         // Resale.
-        ownerThree = new Address();
-        ownerThree.generateKeyPair();
         contract.transfer(ownerTwo.getPK(), ownerThree.getPK(), 1d);
         assertEquals(98d, contract.balanceOf(ownerOne.getPK()), 0d);
         assertEquals(51d, contract.balanceOf(ownerTwo.getPK()), 0d);
         assertEquals(1d, contract.balanceOf(ownerThree.getPK()), 0d);
+
+    }
+    
+    // Verifies that the payable transference did go well.
+    @Test
+    public void test_payable() {
+
+        ownerThree.transferEZI(20d);
+
+        // OwnerThree buy X number of tickets with all EZI of their account (20).
+        contract.payable(ownerThree.getPK(), ownerThree.getBalance());
+        // Because the tickets cost 5 EZI, OwnerThree gets 4 tickets.
+        assertEquals(4d, contract.balanceOf(ownerThree.getPK()), 0d);
+        // Owner's account gets the amount of EZI payed (20).
+        assertEquals(20d, contract.getOwner().getBalance(), 0d);
+
+        // Not enough EZIs to buy a ticket.
+        contract.payable(ownerThree.getPK(), 4d);
+        assertEquals(4d, contract.balanceOf(ownerThree.getPK()), 0d);
+        assertEquals(20d, contract.getOwner().getBalance(), 0d);
+
+        // Attempts to buy one and a half ticket.
+        // OwnerThree gets only one ticket,
+        // and Owner's account gets all the EZI.
+        contract.payable(ownerThree.getPK(), 8d);
+        assertEquals(5d, contract.balanceOf(ownerThree.getPK()), 0d);
+        assertEquals(28d, contract.getOwner().getBalance(), 0d);
 
     }
 
